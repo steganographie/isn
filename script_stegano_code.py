fdd992260c1f950e8c612f04786c03df582674e9
Vitag <vitag.creator@gmail.com>
2020-05-28 15:44:19 +0200

    Mise à jour des commentaires

--- script_stegano_code.py
+++ script_stegano_code.py
@@ -1,32 +1,30 @@
 import shutil
-from tkinter.filedialog import *  # Depuis tkinter import tout le contenu de filedialog
-from PIL import Image  # Depuis le module PIL, importer image, permet d'effectuer des actions sur des images
+from tkinter.filedialog import *
+from PIL import Image
 import os
 
-### IMPORTER L'IMAGE VIA INTERFACE GRAPHIQUE ###
+### Importer l'image via une interface graphique ###
 
 nom_image = askopenfilename(title=u"Ouvrir votre image",filetypes=[('images png','.png'),('image jpg','.jpg'),('images jpeg','.jpeg'),('images ppm','.ppm'),('images bmp','.bmp')])
 #Permet de choisir son fichier via interface graphique et enregistre le chemin sous une variable
 
-print("Le chemin du fichier image est :",nom_image) #Affiche le chemin de l'image , OPTIONNEL
+print("Le chemin du fichier image est :",nom_image) #Affiche le chemin de l'image choisie
 
-img = Image.open(nom_image)
+img = Image.open(nom_image) #Ouvre l'image pour en extraire des propriétés par la suite
 
-#img = Image.open("image.png")  # Enregistre l'image sous la variable nécessite de connaitre l'extension et le chemin de l'image
-## Si on connait a coup sur le chemin de l'image
-### FIN ###
+### Caractéristiques de l'image ###
 
-l, h = img.size  # Renvoie un tupple contenant (largeur,hauteur)
+l, h = img.size  #Renvoie un tupple contenant (largeur,hauteur)
 
-r, g, b = img.split()  # Renvoie un tupple, Crée 3 nouvelles images à partir de celle de base, contenant chacune des bandes originales dans l'ordre rouge, vert et bleu
+r, g, b = img.split()  #Renvoie un tupple, Crée 3 nouvelles images à partir de celle de base, contenant chacune des bandes originales dans l'ordre rouge, vert et bleu
 
-#### CHOISIR DE QUELLE COULEUR AUGMENTER L'INTENSITE ####
+#### Trouver la couleur la plus présente ####
 
-red = list(r.getdata())  # Créée une liste contenant toutes les intensitées de la bande rouge
-green = list(g.getdata())
-blue = list(b.getdata())
+red = list(r.getdata())  #Créée une liste contenant toutes les intensitées de la bande rouge
+green = list(g.getdata()) #Pareil pour le vert
+blue = list(b.getdata()) #et pour le bleu
 
-red_sum = sum(red) #Somme des intensités des rouges
+red_sum = sum(red) #Somme des intensités rouges
 green_sum =sum(green)
 blue_sum = sum(blue)
 
@@ -46,37 +44,34 @@ if taille[0] == blue_sum:
     couleur_acc = list(b.getdata())
     print("Couleur la moins présente : bleu")
 
-#Passer par une somme n'est pas obligatoire mais a du sens car comment dit on qu'une liste et plus forte qu'une autre ? Alors que pour une somme, il faut juste comparer les résultats
-#### FIN ####
+#### Message à encoder ####
 
-c = input("Entrez le texte qui sera encodé dans l'image : ")  # Demande le texte qui sera entré dans l'image
+c = input("Entrez le texte qui sera encodé dans l'image : ")  #Demande le texte qui sera entré dans l'image
 
-u = len(c)  # Renvoie la longeur de la chaine rentrée par l'utilisateur
+u = len(c)  #Renvoie la longeur de la chaine rentrée par l'utilisateur
 
-v = bin(u)[2:].rjust(8, "0")  # On transforme en binaire, met un longeur de 8 bits ATTENTION PAS COMPRIS .rjust
-# [2:] enleve les 2 premiers caractères qui sont 0b, dans Python (pour dire que c'est un nombre binaire)
+v = bin(u)[2:].rjust(8, "0") #Transforme l'entree de l'utilisateur en binaire, ajuste à 8 caractères en ajoutant le caractère "0"
 
 ascii = [bin(ord(i))[2:].rjust(8, "0") for i in c]  # Convertit en binaire la valeur decimale de chaque lettre composant la message que veut coder l'utilisateur dans l'image
 
-### PARTIE COMPLIQUE ###
+### Encodage dans l'image du texte ###
 
-a = ''.join(ascii)  # crée liste qui prends la valeur des 8 bits en rouge
+a = ''.join(ascii)  #Crée une liste qui comporte les valeurs binaires du texte de l'utilisateur
 
 for j in range(8):
-    couleur_acc[j] = 2 * int(couleur_acc[j] // 2) + int(v[j])  # PAS COMPRIS
+    couleur_acc[j] = 2 * int(couleur_acc[j] // 2) + int(v[j])
 
 for k in range(8 * u):
-    couleur_acc[k + 8] = 2 * int(couleur_acc[k + 8] // 2) + int(a[k])
+    couleur_acc[k + 8] = 2 * int(couleur_acc[k + 8] // 2) + int(a[k]) #Cache le texte dans l'image
 
-## FIN ###
+## Création de la nouvelle image ###
 
-couleur_modifiee = Image.new("L", (l,h))  # Creation d'une nouvelle image vide de meme dimensions que celle importée, mode "L" signifie en 8 bits pixels, en noir et blanc
+couleur_modifiee = Image.new("L", (l,h))  # Creation d'une nouvelle image vide de memes dimensions que celle importée, mode "L" signifie en 8 bits pixels, en noir et blanc
 
-couleur_modifiee .putdata(couleur_acc)  # Ajout dans la nouvelle image des teintes rouges modifiées qui contiennent
+couleur_modifiee .putdata(couleur_acc)  # Ajout dans la nouvelle image des teintes rouges modifiées qui contiennent le message de l'utilisateur
 
 if taille[0] == red_sum:
     imgnew = Image.merge('RGB', (couleur_modifiee, g, b))  # Ajoute les bandes bleues et vertes de l'image de base et y ajoute la nouvelle teinte rouge qui contient le texte
-# RGB =  3*8 bits pxiels, true colors
 
 if taille[0] == green_sum:
     imgnew = Image.merge('RGB', (r, couleur_modifiee, b))
@@ -84,16 +79,14 @@ if taille[0] == green_sum:
 if taille[0] == blue_sum:
     imgnew = Image.merge('RGB', (r, g, couleur_modifiee))
 
-### CHOISIR Où ENREGISTRER LA NOUVELLE IMAGE ###
+### Choix de l'emplacement de la nouvelle image ###
 
 nom_repertoire = askdirectory(initialdir="/",title='Choix du répertoire') #demande le repertoire
 
-print("Le répertoire est :",nom_repertoire) #affiche le répertoire pris
+print("Le répertoire est :",nom_repertoire) #Affiche le répertoire pris
 
-imgnew.save("image_avec_message_codé.png") # Enregistre sous le nom choisit la nouvelle image, extension précisée et enregistre dans le dossier ou se trouve le script en python
+imgnew.save("image_avec_message_codé.png") # Enregistre sous le nom choisit la nouvelle image, extension précisée et enregistre dans le dossier où se trouve le script en python
 
 shutil.copy2("image_avec_message_codé.png", nom_repertoire) #fait une copie dans le répertoire que l'utilisateur a pris
 
-os.remove("image_avec_message_codé.png") #supprime le fichier image modifiée dans le dossier ou se trouve le script python
-
-### FIN ###
+os.remove("image_avec_message_codé.png") #supprime le fichier image modifiée dans le dossier où se trouve le script python
--- script_stegano_decode.py
+++ script_stegano_decode.py
@@ -1,15 +1,16 @@
-from tkinter.filedialog import *  # Depuis tkinter import tout le contenu de filedialog
-from PIL import Image  # Depuis le module PIL, importer image, permet d'effectuer des actions sur des images
+from tkinter.filedialog import *
+from PIL import Image
 
 nom_image = askopenfilename(title=u"Ouvrir votre image",filetypes=[('images png','.png'),('images jpeg','.jpeg'),('images ppm','.ppm'),('images bmp','.bmp')])
+#cf ligne 9 programme script_stegano_code.py
 
-im = Image.open(nom_image) #Enregistre l'image sous la variable
+im = Image.open(nom_image) #cf ligne 13 programme script_stegano_code.py
 
-r,g,b=im.split()
+r,g,b=im.split() #Eclate l'image en 3 bandes rouge, vert et bleu
 
-### MODIFICATION DU CODE POUR DETECTER LA BANDE DE COULEUR QUI A CHANGE ###
+### Détection de la couleur modifiée ###
 
-red = list(r.getdata())  # Créée une liste contenant toutes les intensitées de la bande rouge
+red = list(r.getdata())  # Créée une liste contenant toutes les intensités de la bande rouge
 green = list(g.getdata())
 blue = list(b.getdata())
 
@@ -17,41 +18,39 @@ red_sum = sum(red) #Somme des intensités des rouges
 green_sum =sum(green)
 blue_sum = sum(blue)
 
-#### FIN ####
+taille=[red_sum, green_sum, blue_sum] #Liste contenant les valeurs des intensités des 3 couleurs
 
-taille=[red_sum, green_sum, blue_sum] #Liste contenant les valeurs des intensitées des 3 couleurs
-
-#### TROUVER DE QUELLE COULEUR L'INTENSITE A ETE AUGMENTEE ####
-
-taille.sort(reverse=False) #tri dans l'ordre décroissant pour jouer sur la couleur la plus présente (taille[0])
-
-### FIN ##
+taille.sort(reverse=False) #tri dans l'ordre décroissant pour trouver la couleur la plus présente (taille[0])
 
 if taille[0] == red_sum: #si la couleur la DEPEND DU CHOIX PRECEDENT présente est le rouge
     couleur_acc = list(r.getdata()) # alors on modifie l'intensité du rouge
-    print("Couleur la moins présente : rouge")
 
 if taille[0] == green_sum:
     couleur_acc = list(g.getdata())
-    print("Couleur la moins présente : vert")
 
 if taille[0] == blue_sum:
     couleur_acc = list(b.getdata())
-    print("Couleur la moins présente : bleu")
 
 #Passer par une somme n'est pas obligatoire mais a du sens car comment dit on qu'une liste et plus forte qu'une autre ? Alors que pour une somme, il faut juste comparer les résultats
-#### FIN ####
+
+#### Décodage du texte ####
 
 p=[str(x%2) for x in couleur_acc[0:8]]
+
 q="".join(p)
+
 q=int(q,2)
+
 #lecture du message
+
 n=[str(x%2) for x in couleur_acc[8:8*(q+1)]]
 
 b="".join(n)
+
 message=""
 
 for k in range(0,q):
     l=b[8*k:8*k+8]
     message=message+chr(int(l,2))
+
 print ("Le texte encodé dans l'image est :",message)
